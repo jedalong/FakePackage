e93ea5e2e4f8bef7816158497516379219c084c7
Jed Long <jed.a.long@gmail.com>
2021-11-24 14:54:35 -0500

    Initial commit

---
+++ R/jed_add.r
@@ -0,0 +1,22 @@
+
+#' Add two numbers
+#'
+#' Here is a very short description
+#'
+#' Here are some very specific details, like stuff about the inputs.
+#'
+#' @param x First number (numeric)
+#' @param y Second number (numeric)
+#'
+#' @return The sum of x and y (numeric)
+#' @export
+#'
+#' @examples
+#' jed_add(6,7)
+#'
+jed_add = function(x=1, y=1){
+
+  jed = x + y
+  return(jed)
+
+}
---
+++ R/jed_sub.r
@@ -0,0 +1,21 @@
+#' Subtract two numbers
+#'
+#' Here is a very short description
+#'
+#' Here are some very specific details, like stuff about the inputs. Even more specific than last time.
+#'
+#' @param x First number (numeric)
+#' @param y Second number (numeric)
+#'
+#' @return The sum of x and y (numeric)
+#' @export
+#'
+#' @examples
+#' jed_sub(100,44)
+#'
+jed_sub = function(x=1, y=1){
+
+  jed = x - y
+  return(jed)
+
+}
